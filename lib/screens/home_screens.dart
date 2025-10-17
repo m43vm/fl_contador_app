4c6228766d093d1ac60f15a8f8b86d4e4d68d6c4
m43vm <mvazmar0608@g.educaand.es>
2025-10-17 09:50:23 +0200

    fl_contador:app

---
+++ lib/screens/home_screens.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 161, 194, 140),
+      body: Center(child: const Text('AAAAAAAAAAAAAAAAAA')),
+    );
+  }
+}
